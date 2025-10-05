a0ff309d3a7eaf1e46142e4a24ba122451ceb5af
blackbeard45 <vilhoerno@gmail.com>
2025-10-05 08:47:03 +0330

    complete the task - just there is an issue, i should read about changing the urls which are inside the inline css to load every component of the page.

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from landing_page.views import contact_page
+from django.urls import path
 
+app_name = 'landing_page'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('landing_page.urls')),
+    path('', contact_page, name='contact'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
